977b455c4ecb5f8d0d4e31d2c1caac806cfc3083
JowiMesas <joelmesash@gmail.com>
2025-05-07 11:50:40 +0200

    Fix problem with Login

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,13 +1,20 @@
-const BASE ='http://localhost:4000/api';
+const BASE = "http://localhost:4000/api";
 
 export const authService = {
-  login: async (username: string, email: string) =>
-    fetch(`${BASE}/auth/login`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, email })
-    }).then(res => res.json()),
+  login: async (username: string, email: string) => {
+    const res = await fetch(`${BASE}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username, email }),
+    });
 
-  getRooms: async () =>
-    fetch(`${BASE}/auth/salas`).then(res => res.json()),
+    if (!res.ok) {
+      throw new Error(`Error ${res.status}: ${res.statusText}`);
+    }
+
+    return res.json();
+  },
+
+  getRooms: async () => fetch(`${BASE}/auth/salas`).then((res) => res.json()),
 };
+
